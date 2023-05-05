6918af5301d39218415186de997903cbfd7e2052
Daniel <a20180805@pucp.edu.pe>
2023-05-05 17:42:10 -0500

    p3

---
+++ src/main/java/com/example/L5/dto/SalaryDto.java
@@ -0,0 +1,10 @@
+package com.example.L5.dto;
+
+public interface SalaryDto {
+    String getJob_id();
+    String getJob_title();
+    double getMaximo();
+    double getMinimo();
+    double getSalario_total();
+    double getSalario_promedio();
+}
